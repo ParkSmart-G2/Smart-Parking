a588579ca14570cf716e95912548da2f706aedc2
siwarrihane <siwarrihane03@gmail.com>
2023-12-26 19:13:42 +0100

    fixe sign up

--- front-end/src/components/Auth/Signup.js
+++ front-end/src/components/Auth/Signup.js
@@ -4,14 +4,13 @@ import "./Signup.css";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     plateNumber: "",
     email: "",
     password: "",
-    confirmPassword: "",
     phoneNumber: "",
     isAdmin: false,
     isUser: false,
@@ -42,142 +41,152 @@ const SignUp = () => {
       plateNumber,
       email,
       password,
-      confirmPassword, 
       phoneNumber,
       isAdmin,
       isUser,
       codeAdmin,
     } = formData;
 
-    if (!firstName || !lastName || !plateNumber || !email || !password ||
-      !confirmPassword  || !phoneNumber) {
+    if (
+      !firstName ||
+      !lastName ||
+      !plateNumber ||
+      !email ||
+      !password ||
+      !phoneNumber
+    ) {
       console.error("Please fill in all required fields");
       setError("Please fill in all required fields");
       return;
     }
-    if (password !== confirmPassword) {
-      console.error("Password and Confirm Password do not match");
-      setError("Password and Confirm Password fields do not match");
-      
+
+    if (isAdmin && !codeAdmin) {
+      console.error("Please enter the Admin code");
+      setError("Please enter the Admin code");
       return;
     }
 
+    // Check if password and confirmPassword match
+    if (password !== formData.confirmPassword) {
+      console.error("Password and Confirm Password do not match");
+      setError("Password and Confirm Password fields do not match");
+      return;
+    }
 
-
-    
     try {
       let signUpPath = "";
 
       if (isAdmin) {
         signUpPath = "http://localhost:4444/createAdmin";
-        if (!codeAdmin) {
-          console.error("Please enter the Admin code");
-          return;
-        }
       } else {
         signUpPath = "http://localhost:4444/api/auth/register";
       }
 
-      const response = await axios.post(signUpPath, formData);
-      console.log(response.data);
-      window.location.href = '/'
+      // Instead of sending formData, send only the relevant fields
+      const response = await axios.post(signUpPath, {
+        firstName,
+        lastName,
+        plateNumber,
+        email,
+        password,
+        phoneNumber,
+        isAdmin,
+        isUser,
+        codeAdmin,
+      });
 
+      console.log(response.data);
+      window.location.href = "/";
     } catch (error) {
       console.error(error.response.data);
-      setError(error.response.data.message || "An error occurred during registration");
+      setError(
+        error.response.data.message || "An error occurred during registration"
+      );
     }
   };
-console.log('error',error)
+
   return (
-
     <div>
-    <div className="login">
+      <div className="login"></div>
+      <div className="signupContainer">
+        <form onSubmit={handleSubmit} className="signupContainerandSignUp">
+          <h1>Sign Up</h1>
+          {error && <p className="error-message">{error}</p>}
+          <p>
+            you have an account? <Link to="/"> go back to Login</Link>
+          </p>
+          <br />
+          <label>First Name:</label>
+          <input type="text" name="firstName" onChange={handleInputChange} required />
+          <br />
 
-    
-</div >
-    <div className="signup">
-  
+          <label>Last Name:</label>
+          <input type="text" name="lastName" onChange={handleInputChange} required />
+          <br />
 
-            <form onSubmit={handleSubmit} className="signupContainerandSignUp">
-           
-      <h1>Sign Up</h1>
-      
-      {error && <p className="error-message">{error}</p>} 
-      
-      <form onSubmit={handleSubmit} className="signupContainer">
-      <p>you have an account? <Link to="/"> go back to Login</Link></p> 
-      <br />
-        <label>First Name:</label>
-        <input type="text" name="firstName" onChange={handleInputChange} required />
-        <br />
+          <label>Plate Number:</label>
+          <input type="text" name="plateNumber" onChange={handleInputChange} required />
+          <br />
 
-        <label>Last Name:</label>
-        <input type="text" name="lastName" onChange={handleInputChange} required />
-        <br />
+          <label>Email:</label>
+          <input type="email" name="email" onChange={handleInputChange} required />
+          <br />
 
-        <label>Plate Number:</label>
-        <input type="text" name="plateNumber" onChange={handleInputChange} required />
-        <br />
+          <label>Password:</label>
+          <input type="password" name="password" onChange={handleInputChange} required />
+          <br />
 
-        <label>Email:</label>
-        <input type="email" name="email" onChange={handleInputChange} required />
-        <br />
-
-        <label>Password:</label>
-        <input type="password" name="password" onChange={handleInputChange} required />
-        <br />
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          onChange={handleInputChange}
-          required
-        />
-        <br />
-        <label>Contact Number:</label>
-        <input type="text" name="phoneNumber" onChange={handleInputChange} required />
-        <br />
-        <div>
-        <label>
-          Admin:
+          <label>Confirm Password:</label>
           <input
-            type="checkbox"
-            name="isAdmin"
-            checked={formData.isAdmin}
-            onChange={handleCheckboxChange}
+            type="password"
+            name="confirmPassword"
+            onChange={handleInputChange}
+            required
           />
-        </label>
-        <label>   </label>
+          <br />
 
-        <label>
-             User:   
-          <input
-            type="checkbox"
-            name="isUser"
-            checked={formData.isUser}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-        </div>
-        {formData.isAdmin && (
-          <>
-            <br />
-            <label>Admin Code:</label>
-            <input
-              type="text"
-              name="codeAdmin"
-              onChange={handleInputChange}
-              required
-            />
-          </>
-        )}
-        <br />
+          <label>Contact Number:</label>
+          <input type="text" name="phoneNumber" onChange={handleInputChange} required />
+          <br />
 
-        <button type="submit">Sign Up</button>
-      </form>
-      </form>
+          <div>
+            <label>
+              Admin:
+              <input
+                type="checkbox"
+                name="isAdmin"
+                checked={formData.isAdmin}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+            <label>   </label>
+            <label>
+               User:
+              <input
+                type="checkbox"
+                name="isUser"
+                checked={formData.isUser}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+          </div>
+          {formData.isAdmin && (
+            <>
+              <br />
+              <label>Admin Code:</label>
+              <input
+                type="text"
+                name="codeAdmin"
+                onChange={handleInputChange}
+                required
+              />
+            </>
+          )}
+          <br />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     </div>
-    </div >
   );
 };
 
